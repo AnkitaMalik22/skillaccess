02a98b3fa92d72d827713e6cdf5fd337ba40f322
AnkitaMalik22 <ankitamalik936@gmail.com>
2024-04-09 10:34:35 +0530

    qb topics , question add and upload

--- src/components/collage/quesBank/topic/Topic.jsx
+++ src/components/collage/quesBank/topic/Topic.jsx
@@ -1,53 +1,131 @@
 import React, { useState } from "react";
 
-// import { useSelector, useDispatch } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { useEffect } from "react";
 // import { getCompany } from "../../../../redux/collage/dashboard/dashboardSlice";
 import Folder from "../home/icon/Folder";
 import Header from "./Header";
+import { setCurrentTopic,getAllTopics } from "../../../../redux/collage/test/testSlice";
+import { useNavigate } from "react-router-dom";
+
+
 
 const Topic = () => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
   const [topics, settopics] = useState([1, 2, 3, 4, 5, 6, , 9, 6]);
-  // const dispatch = useDispatch();
+  const { sections} = useSelector((state) => state.test);
 
-  // useEffect(() => {
-  //   dispatch(getCompany());
-  // }, [dispatch]);
+  const getTotalQuestions = (topic) => {
+    let total = 0;
+  total = topic?.questions?.length + topic?.video?.length + topic?.compiler?.length + topic?.essay?.length + topic?.findAnswers?.length;
+  
+    return total;
+  }
+  
+  const randomImage = () => {
+    const images = [
+      "../../images/FrontEnd.png",
+      "../../images/HR.png",
+      "../../images/Marketing.png",
+      "../../images/HR.png",
+      "../../images/Marketing.png",
+      "../../images/FrontEnd.png",
+    ];
+    return images[Math.floor(Math.random() * images.length)];
+  }
+
+  const [filteredSections, setFilteredSections] = useState(sections);
+
+  const handleFilterSections = (e) => {
+    const value = e.target.value;
+    if (value === "" || value.trim() === "") {
+      console.log("empty");
+      console.log(filteredSections, "filtered");
+      setFilteredSections(sections);
+
+      return;
+    } else {
+      setFilteredSections(
+        sections.filter((section) => {
+          const regex = new RegExp(value, "i");
+          return regex.test(section.Heading);
+        })
+      );
+
+      console.log(filteredSections, "filtered--", value);
+    }
+  };
+  useEffect(() => {
+    dispatch(getAllTopics());
+
+    // if (sections) {
+    //   setFilteredSections(sections);
+    // }
+
+    try {
+      topics = JSON.parse(localStorage.getItem("topics"));
+
+      // setSelectedSections(topics);
+    } catch (error) {}
+
+    // console.log("hello tests",sections);
+  }, [dispatch]);
 
   return (
     <div className="w-[95%] mx-auto font-dmSans">
-      <Header />
-      <div className="flex flex-wrap gap-4 w-fit justify-center bg-gray-100 px-3 py-6 rounded-3xl">
+      <Header handleFilter={handleFilterSections} />
+      <div className="flex flex-wrap gap-4 w-full justify-center bg-gray-100 px-3 py-6 rounded-3xl ">
         <div className="w-full flex justify-between px-6 mb-2">
           <h2 className="font-bold text-xl">Choose a Topic</h2>{" "}
           <div className="flex">
             {" "}
-            <input
-              type="checkbox"
-              name=""
-              id=""
-              className="bg-[#DEEBFF] rounded border-0 self-center"
-            />
+            {/* <input
+            type="text"
+            placeholder="Search..."
+            name="search"
+            onChange={handleFilterSections}
+            className="placeholder pl-0 border-none self-center bg-gray-100 focus:outline-none focus:ring-0 rounded-e-lg sm:w-80 w-fit"
+          /> */}
             <label className="text-sm pl-1 self-center">Delete Selected</label>
           </div>
         </div>
 
-        {topics &&
-          topics?.map((company, index) => {
+        {filteredSections?.map((section, index) => {
             return (
-              <div className=" w-[17rem] mb-4 bg-white rounded-2xl" key={index}>
+              <div className=" w-[17rem] mb-4 bg-white rounded-2xl" key={index}  onClick={() => {
+                dispatch(
+                  setCurrentTopic({
+                    topic: section
+                      // Type: questionType || "mcq",
+                    ,
+                  })
+                );
+                localStorage.setItem("TopicDetails", JSON.stringify(section));
+                navigate(`/collage/quesBank/topic/${section._id}`);
+              }}
+            >
                 <figure>
-                  <img src="../../images/FrontEnd.png" alt="cover photo" />
+                  <img src={randomImage()} alt="cover" />
                 </figure>
                 <div className="card-body">
-                  <h2 className="font-bold text-xl">Name</h2>
+                  <h2 className="font-bold text-xl">{section?.Heading}</h2>
                   <div className="flex gap-2">
                     <Folder />
-                    <p className="text-[#95ACFA] text-sm">40 Files</p>
+                    <p className="text-[#95ACFA] text-sm">{getTotalQuestions(section)} Files</p>
                   </div>
                 </div>
               </div>
             );
           })}
+
+          {
+            filteredSections.length === 0 && (
+              <div className="w-full flex justify-center items-center  mb-4 bg-white rounded-2xl">
+                <h2 className="text-xl font-bold">No Topics Found</h2>
+              </div>
+            )
+          }
       </div>
     </div>
   );
---
+++ src/pages/collage/quesBank/UploadTopic.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import CollageLayout from '../../../layout/Collage'
+import AddQuestionsSelect from '../../../components/collage/quesBank/home/UploadQuestionsSelect'
+
+const UploadTopic = () => {
+  return (
+  <CollageLayout>
+  <AddQuestionsSelect />
+    </CollageLayout>
+  )
+}
+
+export default UploadTopic
