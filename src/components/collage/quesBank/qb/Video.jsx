5541225c1a0d6fe334366c1e07fb9ec6029b2d8b
AnkitaMalik22 <ankitamalik936@gmail.com>
2024-06-18 15:26:35 +0530

    Refactor VideoMcq component to use dynamic rendering for options

--- src/components/collage/quesBank/qb/Video.jsx
+++ src/components/collage/quesBank/qb/Video.jsx
@@ -26,7 +26,7 @@ const Video = ({ video, Number }) => {
     <Disclosure>
       {({ open }) => (
         <>
-          <div className="flex justify-between gap-3 md:gap-5 font-dmSans relative z-10 mb-4">
+          <div className="flex justify-between gap-3 md:gap-5 font-dmSans relative z-10 mb-4 ">
             {" "}
             <button className=" bg-blued rounded-lg bg-opacity-5 text-left text-blued flex">
               {" "}
@@ -35,85 +35,94 @@ const Video = ({ video, Number }) => {
               </div>
             </button>
             <Disclosure.Button className="w-full bg-gray-100 ">
-              Click to Expand{" "}
-            </Disclosure.Button>
-            {/* <div className="level flex items-center gap-2 bg-gray-100 ">
-              {video.QuestionLevel == "beginner" && (
-                <p className="rounded-2xl mr-4 py-1.5 bg-cyan-500 text-white w-8 h-8 text-center font-extrabold  ">
-                  L1
-                </p>
-              )}
-              {video.QuestionLevel == "intermediate" && (
-                <p className="rounded-2xl mr-4   py-1.5 bg-green-500 text-white w-8 h-8 text-center font-extrabold  ">
-                  L2
-                </p>
-              )}
-              {video.QuestionLevel == "advanced" && (
-                <p className="rounded-2xl mr-4   py-1.5 bg-red-500 text-white w-8 h-8 text-center font-extrabold  ">
-                  L3
-                </p>
-              )}
-            </div> */}
-          </div>
-
-          <Disclosure.Panel  className="relative z-10">
-            
-          <div className="mb-4">
-              <div className="flex w-full justify-between flex-col rounded-lg border-[#95ACFA] border text-[#3E3E3E] py-3 text-left text-lg font-normal  ">
-                <div className="flex justify-between items-center w-full px-2">
-                {/* <h2 className="px-4 font-semibold pt-3 text-base"></h2> */}
-                <div className="px-5 pb-4 flex flex-col gap-4">
-                  <span className="flex gap-2">
-                    <div className="flex w-5 justify-center"></div>
-                    <ReactPlayer url={video?.videoFile} controls />
-
-                    <label for="answer" className="self-center">
-                      {" "}
-                    </label>
-                  </span>
+              <div className="flex justify-between w-full h-full items-center py-2 rounded">
+                <div></div>
+                <h2 className="px-4 font-semibold  text-base">
+                  Click to Expand{" "}
+                </h2>
+                <div className="level flex items-center gap-2 bg-gray-100 ">
+                  {video.QuestionLevel == "beginner" && (
+                    <p className="rounded-2xl mr-4 py-1.5 bg-cyan-500 text-white w-8 h-8 text-center font-extrabold  ">
+                      L1
+                    </p>
+                  )}
+                  {video.QuestionLevel == "intermediate" && (
+                    <p className="rounded-2xl mr-4   py-1.5 bg-green-500 text-white w-8 h-8 text-center font-extrabold  ">
+                      L2
+                    </p>
+                  )}
+                  {video.QuestionLevel == "advanced" && (
+                    <p className="rounded-2xl mr-4   py-1.5 bg-red-500 text-white w-8 h-8 text-center font-extrabold  ">
+                      L3
+                    </p>
+                  )}
                 </div>
               </div>
+            </Disclosure.Button>
+          </div>
 
-              <h1>
-                {video?.questions.length > 0 && "Multiple Choice questions"}
-              </h1>
-              {video?.questions?.length > 0 &&
-                video.questions.map((mcq, index) => {
-                  console.log(mcq);
-                  return (
-                    <VideoMcq
-                      //   handleDelete={handleDelete}
-                      mcq={mcq}
+          <Disclosure.Panel className="relative z-10">
+            <div className="mb-4">
+              <div className="flex w-full justify-between flex-col rounded-lg border-[#95ACFA] border text-[#3E3E3E] py-3 text-left text-lg font-normal  ">
+                <div className="flex justify-between items-center w-full px-2">
+                  {/* <h2 className="px-4 font-semibold pt-3 text-base"></h2> */}
+                  <div className="px-5 pb-4 flex flex-col gap-4">
+                    <span className="flex gap-2">
+                      <div className="flex w-5 justify-center"></div>
+                      <ReactPlayer url={video?.videoFile} controls />
+
+                      <label for="answer" className="self-center">
+                        {" "}
+                      </label>
+                    </span>
+                  </div>
+                </div>
+
+                <h1 className="text-lg font-semibold text-[#3E3E3E] px-4 pt-3">
+                  {video?.questions.length > 0 && "Multiple Choice questions"}
+                </h1>
+                {video?.questions?.length > 0 &&
+                  video.questions.map((mcq, index) => {
+                    console.log(mcq);
+                    return (
+                      <VideoMcq
+                        //   handleDelete={handleDelete}
+                        mcq={mcq}
+                        Number={index}
+                        id={video._id}
+                        video={video}
+                      />
+                    );
+                  })}
+
+                <h1 className="text-lg font-semibold text-[#3E3E3E] px-4 pt-3">
+                  {video?.long.length > 0 && "Long answer questions"}
+                </h1>
+                {video?.long?.length > 0 &&
+                  video.long.map((question, index) => (
+                    <VideoEssayLong
+                      Title={question.Title}
                       Number={index}
                       id={video._id}
                       video={video}
                     />
-                  );
-                })}
-
-              <h1>{video?.long.length > 0 && "Long answer questions"}</h1>
-              {video?.long?.length > 0 &&
-                video.long.map((question, index) => (
-                  <VideoEssayLong
-                    Title={question.Title}
-                    Number={index}
-                    id={video._id}
-                    video={video}
-                  />
-                ))}
-              <h1> {video?.short.length > 0 && "Short answer questions"}</h1>
-              {video?.short?.length > 0 &&
-                video.short.map((question, index) => (
-                  <VideoEssayShort
-                    Title={question.Title}
-                    Number={index}
-                    id={video._id}
-                    video={video}
-                  />
-                ))}
+                  ))}
+                <h1 className="text-lg font-semibold text-[#3E3E3E] px-4 pt-3">
+                  {" "}
+                  {video?.short.length > 0 && "Short answer questions"}
+                </h1>
+                {video?.short?.length > 0 &&
+                  video.short.map((question, index) => (
+                    <VideoEssayShort
+                      Title={question.Title}
+                      Number={index}
+                      id={video._id}
+                      video={video}
+                    />
+                  ))}
+              </div>
             </div>
-            </div>
-            </Disclosure.Panel>
+          </Disclosure.Panel>
         </>
       )}
     </Disclosure>
